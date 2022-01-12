b678d5867abd9efc39d5781d4d0095d92baf1021
0990 <xjlong@outlook.com>
2022-01-12 20:36:11 +0800

    chore:增加goreleaser发布

--- build.bat
+++ build.bat
@@ -1,5 +1,11 @@
 SET GOOS=linux
+SET GOARCH=amd64
 go build -o bin/chinadns cmd/server/main.go
 
 SET GOOS=windows
+SET GOARCH=amd64
 go build -o bin/chinadns.exe cmd/server/main.go
+
+SET GOOS=linux
+SET GOARCH=arm64
+go build -o bin/chinadns_arm cmd/server/main.go
